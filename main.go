d5f368ddbf67d2f0a5a2ee7b64bac2d232999240
zaiyusuf <zainaby1008@gmail.com>
2026-09-23 15:35:25 +0100

    feat: implemented function `addMember` to add a name in wildcard as a new member with 1500 points
    refactor:removed the feature that added member in the `homeHandler` function

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 type Member struct {
@@ -21,20 +22,30 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /{$}", homeHandler)
+	mux.HandleFunc("POST /add/{ID}", addMember)
 
-	log.Println("Starting Server on Port:4040")
+	log.Println("Starting Server on Port:4040 http://localhost:4040")
 	log.Fatal(http.ListenAndServe(":4040", mux))
 
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 
+	for _, val := range members {
+		fmt.Fprintf(w, "%s: %d\n", val.Name, val.Points)
+	}
+
+}
+
+func addMember(w http.ResponseWriter, r *http.Request) {
+	now :=string(time.Now().Format("02-01-2006 03:04PM"))
+	name := r.PathValue("ID")
 	members[id] = Member{
-		Name:   "Zainab",
+
+		Name:   name,
 		Points: 1500,
+		LastUpdated: now,
+
 	}
 	id++
-	for _, val := range members {
-		fmt.Fprintf(w, "%s: %d", val.Name, val.Points)
-	}
 }
